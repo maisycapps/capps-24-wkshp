4352971eaecf43aaa256292182372c2442809d13
Maisy Capps <maisycapps@gmail.com>
2024-08-19 14:25:06 -0700

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,33 @@ import {puppyList} from './data.js'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList) 
+  const [featPupId, setFeatPupId] = useState(null)
 
-  console.log(puppies);
+  const featuredPup = puppies.find((pup) => {
+   return pup.id === featPupId
+  })
+  console.log(featuredPup);
 
   return (
     <>
       <div className="App">
+        <div className='List'>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p className="puppyPs" key={puppy.id} onClick={() => { setFeatPupId(puppy.id)}}>{puppy.name}</p>
           })
         }
+        </div>
+        {featPupId ? (
+        <div className='Feat'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li><b>Age:</b> {featuredPup.age}</li>
+            <li><b>Email:</b> <a href="featuredPup.email">{featuredPup.email}</a></li>
+          </ul>
+        </div>
+      ) : <h2>Please Select a Dog</h2> }
+
       </div>
     </>
   )
